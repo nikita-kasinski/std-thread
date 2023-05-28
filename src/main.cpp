058606c36dff7edf7a9275aa74d8e53c76cc1a44
nikita-kasinski <kasinski.nikita@gmail.com>
2023-05-28 11:59:53 +0300

    add elapsed time counting, add output information

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,31 @@
 #include <iomanip>
 #include <ranges>
 #include <list>
+#include <future>
+#include <chrono>
 
 #include "matrix.h"
 #include "matrix-utils.h"
 
 int main()
 {
-    std::size_t columns = 3;
-    Matrix<int> a = generate<int>(3, 3);
-    Matrix<int> b = generate<int>(3, columns);
+    std::size_t columns = 10;
+    std::size_t rows = columns;
+    Matrix<int> a = generate<int>(rows, columns);
+    Matrix<int> b = generate<int>(rows, columns);
     std::cout << a << "\n"
               << b;
     for (auto numberOfThreads : std::ranges::iota_view(1u, columns + 1))
     {
         std::list<std::thread> threads;
-        Matrix<int> result(3, 3);
+        Matrix<int> result(rows, columns);
         std::vector<std::list<std::size_t>> columnsByThread(numberOfThreads, std::list<std::size_t>());
         for (auto column : std::ranges::iota_view(0u, columns))
         {
             columnsByThread[column % numberOfThreads].push_back(column);
         }
+        std::cout << "Number of threads: " << numberOfThreads << "\n";
+        std::cout << "Columns partition: \n";
         for (const auto& columnsList : columnsByThread)
         {
             for (const auto& column : columnsList)
@@ -33,20 +38,37 @@ int main()
             }
             std::cout << "\n";
         }
+        auto begin = std::chrono::steady_clock::now();
         for (auto thread : std::ranges::iota_view(0u, numberOfThreads))
         {
             threads.emplace_back(
-                multiplyColumns<int>,
+                [](
+                    std::list<std::size_t> columns,
+                    const Matrix<int>& a,
+                    const Matrix<int>& b,
+                    Matrix<int>& result
+                ){
+                    multiplyColumns<int>(
+                        columns,
+                        a,
+                        b,
+                        result);
+                },
                 std::cref(columnsByThread[thread]),
                 std::cref(a),
                 std::cref(b),
-                std::ref(result));
+                std::ref(result)
+                );
         }
         for (auto& thread : threads)
         {
             thread.join();
         }
-        std::cout << result;
+        auto end = std::chrono::steady_clock::now();
+        auto time_ns = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        std::cout << "Time elapsed: " << time_ns.count() << " microseconds\n";
+        std::cout << "Result:\n" << result << "\n"; 
+        std::cout.flush();
     }
     return 0;
 }
